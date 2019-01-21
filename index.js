3f78aab1100c47155edfc8e09c03858ce3403b64
charlie  lee <ctlee9096@gmail.com>
2019-01-21 20:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function createPost (){
   function postComment() {
-  let commenter = document.getElementById('commenterName').value;
+  let title = document.getElementById('title').value;
   let comment = document.getElementById('commentText').value;
   //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+  //<div class="comment"><p>comment</p><p>Posted By: 
+  //<span class="title">title</span></p></div>
 }
 }
