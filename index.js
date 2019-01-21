5bb09cce074f159d03df4bac290b64349f1f04c1
charlie  lee <ctlee9096@gmail.com>
2019-01-21 21:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function createPost (){
-  let page = document.getElementById('page-template').value;
-  let poster = document.getElementById('poster-template').value;
+  let pageTemplate = document.getElementById('page-template').value;
+  let posterTemplate = document.getElementById('poster-template').value;
   //insert poster into "posters" div in this format:
   //<div class="poster"><p>poster</p><p>Posted By:
   //<span class="title">title</span></p></div>
