7d59c6819ea58ff6dc509cf4dc65bee2395854c8
charlie  lee <ctlee9096@gmail.com>
2019-01-21 20:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function createPost (){
   let title = document.getElementById('title').value;
   let comment = document.getElementById('commentText').value;
   //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: 
+  //<div class="comment"><p>comment</p><p>Posted By:
   //<span class="title">title</span></p></div>
 }
 }
