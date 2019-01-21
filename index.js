24658958cdaf89d0cc6ea2b278a380fede2df4f5
charlie  lee <ctlee9096@gmail.com>
2019-01-21 21:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function createPost (){
-  let pageTemplate = document.getElementById('page-template').innerHTML;
-  let posterTemplate = document.getElementById('poster-template').innerHTML;
-  let commentsTemplate = document.getElementById('comments-template').innerHTML;
-  //insert pageTemplate into "pageTemplate" div in this format:
-  //<div class="pageTemplate"><p>poster</p><p>Posted By:
-  //<span class="title">title</span></p></div>
+function createPost() {
+  let pageTemplate=_.template(document.getElementById("page-template").innerHTML);
+  let postTemplate=_.template(document.getElementById("post-template").innerHTML);
+  let commentsTemplate=_.template(document.getElementById("comments-template").innerHTML);
 
+  let postTitle=document.getElementById("postTitle").value;
+  let postBody=document.getElementById("postBody").value;
+  let postAuthor=document.getElementById("postAuthor").value;
+
+  document.getElementsByTagName("main")[0].innerHTML+=pageTemplate();
+
+  let blogSection = postTemplate({ 'title': postTitle, 'body': postBody, 'poster': postAuthor });
+  let commentsSection = commentsTemplate();
+
+  let postElement = document.getElementById("post");
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
-function postComment (){
+
+function postComment(){
   let commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
-  
+  let commenterName = document.getElementById('commenter').value;
+  let commentText = document.getElementById('commentText').value;
+  let commentsSection = document.getElementById('comments');
+  commentsSection.innerHTML+=commentTemplate({ "comment": commenterName, "commenter": commentText });
+
 }
